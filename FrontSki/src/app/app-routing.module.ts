d8ac3eb2542d9e1e235c9d46ab64f07b93f214a7
Amir0055 <93008466+Amir0055@users.noreply.github.com>
2023-11-16 03:00:14 +0100

    Update app-routing.module.ts

--- FrontSki/src/app/app-routing.module.ts
+++ FrontSki/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SubscriptionComponent } from './subscription/subscription.component';
 
 const routes: Routes = [
-  { path :"", redirectTo:"add-subscription", pathMatch:"full"}
+  { path :"", redirectTo:"add-subscription", pathMatch:"full"},
   { path: 'add-subscription', component: SubscriptionComponent }
 // Add other routes as needed
  // { path: '', redirectTo: '/add-subscription', pathMatch: 'full' },  Redirect to add-subscription by default];
